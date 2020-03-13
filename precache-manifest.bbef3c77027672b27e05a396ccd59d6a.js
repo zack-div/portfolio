823e36de452392b4aebb69e2c29b6ecb52b825b2
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2020-03-13 13:40:11 +0100

    Updates

---
+++ precache-manifest.bbef3c77027672b27e05a396ccd59d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c401370e8b55f26b80c0d912866fb0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05d47f90818410e31c49",
+    "url": "/portfolio/static/css/main.d4b548b2.chunk.css"
+  },
+  {
+    "revision": "1697edbd8e3cf7f95e60",
+    "url": "/portfolio/static/js/2.86ffb355.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/portfolio/static/js/2.86ffb355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d47f90818410e31c49",
+    "url": "/portfolio/static/js/main.62cdd836.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
